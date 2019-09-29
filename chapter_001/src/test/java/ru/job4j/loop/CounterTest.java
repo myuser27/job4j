57fd337c73fffccf6aa56998d74615c43d91a0be
myuser27 <al_mail@mail.ru>
2019-09-29 11:56:48 +1000

    Подсчет суммы четных чисел в диапазоне [#183249]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest
+{
+    @Test
+    public void whenSum1To10()
+    {
+        int check=Counter.add(1,10);
+        assertThat(check,is(30));
+    }
+}

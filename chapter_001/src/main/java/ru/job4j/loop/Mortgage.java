ce587fc1935b7d7723e69982743963eef9fc9f07
myuser27 <al_mail@mail.ru>
2019-09-30 09:08:28 +1000

    Ипотека [#183256]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage
+{
+    public int calc(int amount, int salary,double percent)
+    {
+        int year=1;
+        double rest=amount+amount*percent/100;
+        rest-=salary;
+        while(rest>0)
+        {
+            rest+=rest*percent/100;
+            rest-=salary;
+            year++;
+        }
+        return year;
+    }
+}

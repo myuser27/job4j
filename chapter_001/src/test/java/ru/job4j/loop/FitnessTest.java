128d263f31af83ab03d61101f3d90f84918c756e
myuser27 <al_mail@mail.ru>
2019-09-29 15:19:43 +1000

    Протеиновая диета [#183254]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FitnessTest
+{
+    @Test
+    public void whenIvanGreatNik()
+    {
+        Fitness fit=new Fitness();
+        int mon=fit.calc(95,90);
+        assertThat(mon,is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik()
+    {
+        Fitness fit=new Fitness();
+        int mon=fit.calc(90,95);
+        assertThat(mon,is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik()
+    {
+        Fitness fit=new Fitness();
+        int mon=fit.calc(50,95);
+        assertThat(mon,is(2));
+    }
+}

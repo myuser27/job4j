1af7766cda44af41418f0c4c81d4ac38c952fefb
myuser27 <al_mail@mail.ru>
2019-09-30 15:34:48 +1000

    Поиск минимума в диапазоне [#183275]

---
+++ chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MinDiapason
+{
+    public static int findMin(int [] arr, int start, int finish)
+    {
+        int min=arr[start];
+        for(int i=start+1;i<=finish;i++)
+        {
+            if(arr[i]<min)
+                min=arr[i];
+        }
+        return min;
+    }
+}

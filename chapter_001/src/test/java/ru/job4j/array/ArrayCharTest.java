74998a7721e7b8870b9bf00910fdc69c82995415
myuser27 <al_mail@mail.ru>
2019-09-30 14:50:28 +1000

    Слово начинается с ... [#183263]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ArrayCharTest
+{
+    @Test
+    public void whenStartStringTrue()
+    {
+        char[] word={'H', 'e', 'l', 'l', 'o'};
+        char[] pref={'H', 'e'};
+        boolean result=ArrayChar.startString(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartStringFalse()
+    {
+        char[] word={'H', 'e', 'l', 'l', 'o'};
+        char[] pref={'H', 'i'};
+        boolean result=ArrayChar.startString(word, pref);
+        assertThat(result, is(false));
+    }
+}

b82f5e8583a114d7a9a4532974dd62b05b15b822
myuser27 <al_mail@mail.ru>
2019-09-30 16:50:10 +1000

    JavaDoc дополнение [#183252]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,24 +8,44 @@ package ru.job4j.calculator;
 
 public class Calculator
 {
+    /**
+     * add - сложение двух чисел
+     * @param first - первое число
+     * @param second - второе число
+     */
     public static void add(double first, double second)
     {
         double result=first+second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * div - деление двух чисел
+     * @param first - делимое
+     * @param second - делитель
+     */
     public static void div(double first, double second)
     {
         double result=first/second;
         System.out.println(first + "/" + second + " = " + result);
     }
 
+    /**
+     * multiply - умножение двух чисел
+     * @param first - первое число
+     * @param second - второе число
+     */
     public static void multiply(double first, double second)
     {
         double result=first*second;
         System.out.println(first + "*" + second + " = " + result);
     }
 
+    /**
+     * subtrack - вычитание второго числа из первого
+     * @param first - уменьшаемое
+     * @param second - вычитаемое
+     */
     public static void subtrack(double first, double second)
     {
         double result=first-second;

4ffa6ea18781d66fcaec3c588bd2d1fc247acaf7
myuser27 <al_mail@mail.ru>
2019-09-30 12:08:35 +1000

    Заполнить массив степенями чисел [#183260]

---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SquareTest
+{
+    @Test
+    public void whenBound3()
+    {
+        Square square=new Square();
+        int[] rst=square.calculate(3);
+        int[] expect=new int[] {1,4,9};
+        assertThat(rst,is(expect));
+    }
+
+    @Test
+    public void whenBound4()
+    {
+        Square square=new Square();
+        int[] rst=square.calculate(4);
+        int[] expect=new int[] {1,4,9,16};
+        assertThat(rst,is(expect));
+    }
+
+    @Test
+    public void whenBound5()
+    {
+        Square square=new Square();
+        int[] rst=square.calculate(5);
+        int[] expect=new int[] {1,4,9,16,25};
+        assertThat(rst,is(expect));
+    }
+}

98f53c2f2492ba4af6709fc598ab553d4376f4a8
myuser27 <al_mail@mail.ru>
2019-10-11 22:06:19 +1000

    Singleton изменения тестов 2 [#183380]

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleTest.java
@@ -1,41 +1,34 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class TrackerSingleTest {
     @Test
     public void whereSingleton1() {
         TrackerSingle1.TrackerSingle tracker1 = TrackerSingle1.TrackerSingle.INSTANCE;
         TrackerSingle1.TrackerSingle tracker2 = TrackerSingle1.TrackerSingle.INSTANCE;
-        if(tracker1 == tracker2) {
-            System.out.println("Singl object");
-        }
+        assertTrue(tracker1 == tracker2);
     }
 
     @Test
     public void whereSingleton2() {
         TrackerSingle2 tracker1 = TrackerSingle2.getInstance();
         TrackerSingle2 tracker2 = TrackerSingle2.getInstance();
-        if(tracker1 == tracker2) {
-            System.out.println("Singl object");
-        }
+        assertTrue(tracker1 == tracker2);
     }
 
     @Test
     public void whereSingleton3() {
         TrackerSingle3 tracker1 = TrackerSingle3.getInstance();
         TrackerSingle3 tracker2 = TrackerSingle3.getInstance();
-        if(tracker1 == tracker2) {
-            System.out.println("Singl object");
-        }
+        assertTrue(tracker1 == tracker2);
     }
 
     @Test
     public void whereSingleton4() {
         TrackerSingle4 tracker1 = TrackerSingle4.getInstance();
         TrackerSingle4 tracker2 = TrackerSingle4.getInstance();
-        if(tracker1 == tracker2) {
-            System.out.println("Singl object");
-        }
+        assertTrue(tracker1 == tracker2);
     }
 }

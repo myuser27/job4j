c41136baca12f80f952424ea55873d9091c1a4dd
myuser27 <al_mail@mail.ru>
2019-10-13 22:41:56 +1000

    1. Телефонный справочник на базе ArrayList. [#183332]

---
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for(Person p : persons) {
+            if(p.getName().contains(key) || p.getSurname().contains(key) ||
+                    p.getAddress().contains(key) || p.getPhone().contains(key)) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+}

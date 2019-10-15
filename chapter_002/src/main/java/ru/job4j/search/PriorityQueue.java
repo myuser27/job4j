229d5e9bb55329179835e319a716897699e4f2e3
myuser27 <al_mail@mail.ru>
2019-10-15 10:45:56 +1000

    2. Очередь с приоритетом на LinkedList исправлено [#183333]

--- chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,21 +1,26 @@
 package ru.job4j.search;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        if (tasks.isEmpty()) {
-            tasks.add(task);
+        if (this.tasks.isEmpty()) {
+            this.tasks.add(task);
         } else {
-            for (Task t : tasks) {
+            int count = 0;
+            for (Task t : this.tasks) {
                 if (t.getPriority() > task.getPriority()) {
-                    tasks.add(tasks.indexOf(t), task);
+                    this.tasks.add(this.tasks.indexOf(t), task);
                     break;
                 }
+                count++;
             }
-            tasks.addLast(task);
+            if (count == this.tasks.size()) {
+                this.tasks.addLast(task);
+            }
         }
     }
 

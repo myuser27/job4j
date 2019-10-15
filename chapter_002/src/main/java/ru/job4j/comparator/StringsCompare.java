4ca39885d616dca024ce575980eaaca279d7f0b7
myuser27 <al_mail@mail.ru>
2019-10-15 16:35:07 +1000

    3. Компаратор для строк [#183323]

---
+++ chapter_002/src/main/java/ru/job4j/comparator/StringsCompare.java
@@ -0,0 +1,22 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+public class StringsCompare implements Comparator<String> {
+    @Override
+    public int compare(String first, String second) {
+        int result = 0;
+        int minlen = first.length() <= second.length()
+                ? first.length() : second.length();
+        for (int i = 0; i < minlen; i++) {
+            result = Integer.compare((int) first.charAt(i), (int) second.charAt(i));
+            if (result != 0) {
+                break;
+            }
+        }
+        if (result == 0 && second.length() > first.length()) {
+            result = -1;
+        }
+        return result;
+    }
+}

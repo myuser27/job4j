b0b39ba70e21f9baaee44990263e8d9585ad5c0c
myuser27 <al_mail@mail.ru>
2019-10-23 14:20:20 +1000

    4. Преобразование List в Map. [#183216]

--- chapter_002/src/main/java/ru/job4j/school/School.java
+++ chapter_002/src/main/java/ru/job4j/school/School.java
@@ -1,14 +1,17 @@
 package ru.job4j.school;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
     public static class Student {
+        private String name;
         private int score;
 
-        public Student(int score) {
+        public Student(String name, int score) {
+            this.name = name;
             this.score = score;
         }
 
@@ -20,7 +23,8 @@ public class School {
         public boolean equals(Object obj) {
             boolean result = false;
             Student other = (Student) obj;
-            if (this == obj || this.score == other.score) {
+            if (this == obj || (this.score == other.score
+                    && this.name.equals(other.name))) {
                 result = true;
             }
             return result;
@@ -28,11 +32,16 @@ public class School {
 
         @Override
         public int hashCode() {
-            return 17 * this.score;
+            return 17 * this.score + this.name.hashCode();
         }
     }
 
     public List<Student> collect(List<Student> students, Predicate<Student> pred) {
         return students.stream().filter(pred).collect(Collectors.toList());
+    }
+
+    public Map<String, Student> convert(List<Student> students) {
+        return students.stream().distinct()
+                .collect(Collectors.toMap(s -> s.name, s -> s));
     }
 }
--- chapter_002/src/test/java/ru/job4j/school/SchoolTest.java
+++ chapter_002/src/test/java/ru/job4j/school/SchoolTest.java
@@ -3,6 +3,7 @@ package ru.job4j.school;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.junit.Assert.assertThat;
@@ -12,56 +13,70 @@ public class SchoolTest {
     @Test
     public void whereClass10C() {
         List<School.Student> students = List.of(
-                new School.Student(40),
-                new School.Student(45),
-                new School.Student(49),
-                new School.Student(55),
-                new School.Student(65),
-                new School.Student(90),
-                new School.Student(95),
-                new School.Student(100));
+                new School.Student("name1",40),
+                new School.Student("name2",45),
+                new School.Student("name3",49),
+                new School.Student("name4",55),
+                new School.Student("name5",65),
+                new School.Student("name6",90),
+                new School.Student("name7",95),
+                new School.Student("name8",100));
         Predicate<School.Student> classC = p -> p.getScore() < 50;
         List<School.Student> expect = List.of(
-                new School.Student(40),
-                new School.Student(45),
-                new School.Student(49));
+                new School.Student("name1",40),
+                new School.Student("name2",45),
+                new School.Student("name3",49));
         assertThat(new School().collect(students, classC), is(expect));
     }
 
     @Test
     public void whereClass10B() {
         List<School.Student> students = List.of(
-                new School.Student(40),
-                new School.Student(45),
-                new School.Student(49),
-                new School.Student(55),
-                new School.Student(65),
-                new School.Student(90),
-                new School.Student(95),
-                new School.Student(100));
+                new School.Student("name1",40),
+                new School.Student("name2",45),
+                new School.Student("name3",49),
+                new School.Student("name4",55),
+                new School.Student("name5",65),
+                new School.Student("name6",90),
+                new School.Student("name7",95),
+                new School.Student("name8",100));
         Predicate<School.Student> classB = p -> (p.getScore() < 70) && (p.getScore() >= 50);
         List<School.Student> expect = List.of(
-                new School.Student(55),
-                new School.Student(65));
+                new School.Student("name4",55),
+                new School.Student("name5",65));
         assertThat(new School().collect(students, classB), is(expect));
     }
 
     @Test
     public void whereClass10A() {
         List<School.Student> students = List.of(
-                new School.Student(40),
-                new School.Student(45),
-                new School.Student(49),
-                new School.Student(55),
-                new School.Student(65),
-                new School.Student(90),
-                new School.Student(95),
-                new School.Student(100));
+                new School.Student("name1",40),
+                new School.Student("name2",45),
+                new School.Student("name3",49),
+                new School.Student("name4",55),
+                new School.Student("name5",65),
+                new School.Student("name6",90),
+                new School.Student("name7",95),
+                new School.Student("name8",100));
         Predicate<School.Student> classA = p -> p.getScore() >= 70;
         List<School.Student> expect = List.of(
-                new School.Student(90),
-                new School.Student(95),
-                new School.Student(100));
+                new School.Student("name6",90),
+                new School.Student("name7",95),
+                new School.Student("name8",100));
         assertThat(new School().collect(students, classA), is(expect));
+    }
+
+    @Test
+    public void whereStudentsListToMap() {
+        List<School.Student> students = List.of(
+                new School.Student("name1",40),
+                new School.Student("name2",45),
+                new School.Student("name3",49));
+        Predicate<School.Student> classC = p -> p.getScore() != 0;
+        Map<String, School.Student> expect = Map.of(
+                "name1", new School.Student("name1",40),
+                "name2", new School.Student("name2",45),
+                "name3", new School.Student("name3",49));
+        assertThat(new School().convert(students), is(expect));
     }
 }

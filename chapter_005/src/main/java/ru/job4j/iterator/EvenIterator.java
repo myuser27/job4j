14d999be4b545f2baf903b3c39e635a8f3676cda
myuser27 <al_mail@mail.ru>
2019-11-04 17:04:42 +1000

    5.1.2. Создать итератор четные числа - изменил [#193408]

--- chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -15,47 +15,24 @@ public class EvenIterator implements Iterator {
         return this.numbers[index] % 2 == 0;
     }
 
-    public int getIndexNextEven(int index) {
-        int result = -1;
-        if (this.numbers.length > 1) {
-            index++;
-            while (this.numbers.length > index) {
-                if (isEven(index)) {
-                    result = index;
-                    break;
-                }
-                index++;
-            }
-        } else if (isEven(index)) {
-            result = index;
-        }
-        return result;
-    }
-
     @Override
     public boolean hasNext() {
         boolean result = false;
-        if (this.numbers.length > 1) {
-            result = getIndexNextEven(this.index) > 0
-                    || (this.index == this.numbers.length - 1 && isEven(this.index)) ? true : false;
-        } else {
-            result = this.index == 0 && isEven(this.index) ? true : false;
+        for (int i = this.index; i < this.numbers.length; i++) {
+            if (isEven(i)) {
+                result = true;
+                this.index = i;
+                break;
+            }
         }
         return result;
     }
 
     @Override
     public Object next() {
-        int result = 0;
-        if (!isEven(index)) {
-            this.index = getIndexNextEven(this.index);
-        }
-        try {
-            result = this.numbers[this.index];
-            this.index++;
-        } catch (ArrayIndexOutOfBoundsException a) {
+        if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return result;
+        return this.numbers[index++];
     }
 }

68df291b3b9d7dafc2dd4f88a6fe92bcb82291e1
myuser27 <al_mail@mail.ru>
2019-11-24 18:35:06 +1000

    5.3.3.1 Очередь на двух стеках - изменил [#193393]

--- chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -1,13 +1,27 @@
 package ru.job4j.list;
 
 public class SimpleQueue<T> {
-    private LinkedContainer stack = new LinkedContainer();
+    private SimpleStack stack = new SimpleStack();
+    private SimpleStack queue = new SimpleStack();
+    private int count = 0;
 
     public void push(T value) {
-        stack.add(value);
+        if (this.count == 0) {
+            this.queue.push(value);
+            this.count++;
+        } else {
+            for (int i = 1; i < this.count + 1; i++) {
+                this.stack.push(this.queue.poll());
+            }
+            this.stack.push(value);
+            this.count++;
+            for (int i = 1; i < this.count + 1; i++) {
+                this.queue.push(this.stack.poll());
+            }
+        }
     }
 
     public T poll() {
-        return (T) stack.deleteFirst();
+        return (T) this.queue.poll();
     }
 }

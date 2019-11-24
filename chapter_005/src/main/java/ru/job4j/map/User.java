759271482d897b7e192f384206e9747c893dbdc2
myuser27 <al_mail@mail.ru>
2019-11-24 19:14:09 +1000

    2. Не перекрывать equals и hashCode [#193401]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     String name;
@@ -11,5 +14,17 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 10,
+                new GregorianCalendar(2009, Calendar.JANUARY, 12));
+        User user2 = new User("Ivan", 10,
+                new GregorianCalendar(2009, Calendar.JANUARY, 12));
+        Map<User, String> map = new HashMap<User, String>();
+        map.put(user1, "first user");
+        map.put(user2, "second user");
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
     }
 }

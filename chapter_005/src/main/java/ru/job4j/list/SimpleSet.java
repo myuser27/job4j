7d922cbecec8f341e9e00058309efb07ccba9866
myuser27 <al_mail@mail.ru>
2019-11-24 18:45:39 +1000

    1. Реализовать коллекцию Set на массиве - исправил проверку на null [#193417]

--- chapter_005/src/main/java/ru/job4j/list/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleSet.java
@@ -19,9 +19,10 @@ public class SimpleSet<E> {
 
     public boolean isDublicate(E value) {
         boolean result = false;
-        if (this.count > 0 && value != null) {
+        if (this.count > 0) {
             for (int i = 0; i < this.count; i++) {
-                if (this.set.get(i) != null && this.set.get(i).equals(value)) {
+                if (this.set.get(i) == value
+                        || (this.set.get(i) != null && this.set.get(i).equals(value))) {
                     result = true;
                     break;
                 }
@@ -32,18 +33,5 @@ public class SimpleSet<E> {
 
     public Iterator<E> iterator() {
         return this.set.iterator();
-    }
-
-    public static void main(String[] args) {
-        SimpleSet set = new SimpleSet(3);
-        set.add("1");
-        set.add(null);
-        set.add("2");
-        set.add("1");
-        Iterator it = set.iterator();
-        System.out.println(it.next());
-        System.out.println(it.next());
-        System.out.println(it.next());
-        System.out.println(it.hasNext());
     }
 }

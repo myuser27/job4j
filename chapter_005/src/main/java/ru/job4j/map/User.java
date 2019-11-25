c2ca9818b3d5f593d9b9c484a5b06e4b54a2ab74
myuser27 <al_mail@mail.ru>
2019-11-25 21:34:11 +1000

    1. Создать модель User - поля privat [#193397]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
